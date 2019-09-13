5baebdca5429b085fa3e1819daf5a7a5cb24d881
David Benton <davidbenton@pmacdiag.ccci.org>
2019-09-13 17:09:14 -0400

    removed jquery3 from application.js due to heroku compiling error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("jquery3")
+// require("jquery3")
 require("popper")
 require("bootstrap-sprockets")
 
