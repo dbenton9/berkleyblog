d852932a2a49976df033b7adc5039a6436d901eb
David Benton <davidbenton@pmacdiag.ccci.org>
2019-09-20 11:41:04 -0400

    updated bootstrap to work with rails 6

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,14 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("bootstrap/dist/js/bootstrap")
+
+//= require jquery3
+//= require popper
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+//= require bootstrap-sprockets
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,8 +24,4 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-//= require jquery
-//= require jquery-ujs
-//= require popper
-//= require bootstrap-sprockets
-//= require_tree .
+
