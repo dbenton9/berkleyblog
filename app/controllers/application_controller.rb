e580628f2644fa76bc0c64f801d8d573ecbdc2c2
David Benton <davidbenton@pmacdiag.ccci.org>
2019-10-02 16:31:57 -0400

    admin access and priveleges restricted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
 
     helper_method :current_user
 
+    # method hiding privileged access pages
+    def not_found
+        raise ActionController::RoutingError.new('Not Found')
+    end
+
     # if logged in sets current user
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -17,17 +22,17 @@ class ApplicationController < ActionController::Base
 
     # checks to see if admin
     def require_admin
-        redirect_to '/' unless current_user.admin?
+        not_found unless current_user.admin?
     end
 
     # checks to see if editor
     def require_editor
-        redirect_to '/' unless current_user.editor? || current_user.admin?
+        not_found '/' unless current_user.editor? || current_user.admin?
     end
 
     # checks to see if subscriber
     def require_subscriber
-        redirect_to '/' unless current_user.subscriber? || current_user.editor? || current_user.admin?
+        not_found '/' unless current_user.subscriber? || current_user.editor? || current_user.admin?
     end
 
 end
